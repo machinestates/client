3fc27b8bc6e3e00b690ec29873028ee76a6af343
Erik Johnson <erik@erikaugust.com>
2023-07-04 21:12:02 -0400

    M2-66: Creates hacked modal with images

--- src/app/components/game-hacked-modal/game-hacked-modal.component.ts
+++ src/app/components/game-hacked-modal/game-hacked-modal.component.ts
@@ -9,15 +9,27 @@ import { ModalController } from '@ionic/angular';
 export class GameHackedModalComponent implements OnInit {
 
   @Input() amount: number;
+  image: string = 'assets/images/hacked-image0.jpg';
 
   constructor(private modalController: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.randomizeImage();
+  }
 
   close() {
     this.modalController.dismiss({
       dismissed: true
     });
+  }
+
+  randomizeImage() {
+    const images = [
+      'assets/images/hacked-image0.jpg',
+      'assets/images/hacked-image1.jpg',
+      'assets/images/hacked-image2.jpg'
+    ];
+    this.image = images[Math.floor(Math.random() * images.length)];
   }
 
 }
