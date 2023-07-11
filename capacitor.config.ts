f05578a3be5606461be4e8e87c5065bfde848976
Erik Johnson <erik@erikaugust.com>
2023-07-11 10:32:55 -0400

    Updates display name, bundle identifier, and orientation settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Machinestates',
+  appId: 'com.machinestates.app',
+  appName: 'Machine States',
   webDir: 'www',
   server: {
     androidScheme: 'https'
