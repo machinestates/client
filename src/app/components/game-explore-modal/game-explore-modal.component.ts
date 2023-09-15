bf3a08725949b845e696131b2115338ff7bc69cb
Erik Johnson <erik@erikaugust.com>
2023-09-15 08:12:25 -0400

    M2-143: Adds typing plugin for modal description content (M2-153)

--- src/app/components/game-explore-modal/game-explore-modal.component.ts
+++ src/app/components/game-explore-modal/game-explore-modal.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import Typed, { TypedOptions } from 'typed.js';
+
 
 @Component({
   selector: 'app-game-explore-modal',
   templateUrl: './game-explore-modal.component.html',
   styleUrls: ['./game-explore-modal.component.scss'],
 })
-export class GameExploreModalComponent  implements OnInit {
+export class GameExploreModalComponent  implements OnInit, AfterViewInit {
   @Input() found: any;
+  @ViewChild('wrapper') private content;
 
   constructor(private modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.initializeValues();
+  }
+
+  initializeValues() {
+    new Typed(this.content.nativeElement.querySelector('.typing'), {
+      strings: [this.found.description],
+      typeSpeed: 50
+    });
   }
 
   close() {
